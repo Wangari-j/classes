9e9a59ff52eb49ce074679be268c0e676e2c7154
Wangari-j <wangarijudy@gmail.com>
2024-02-08 23:21:50 +0300

    classes

---
+++ classes.py
@@ -0,0 +1,15 @@
+class Person:
+    def  __init__(self, fname, lname):
+         self.fname = fname
+         self.lname = lname
+
+newPerson = Person("Juma", "Baraka")
+print(newPerson)
+print(newPerson.fname)
+print(newPerson.lname)
+
+
+
+
+
+    
